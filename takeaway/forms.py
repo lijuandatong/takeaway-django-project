f6cb0e0c3e6a9802670315fe4b861c416487140b
Lijuan Wang <2738042w@student.gla.ac.uk>
2023-03-06 01:14:25 +0000

    update login and signup pages

---
+++ takeaway/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.models import User
+
+from takeaway.models import UserProfile
+
+
+class UserForm(forms.ModelForm):
+    # overwrite password
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
