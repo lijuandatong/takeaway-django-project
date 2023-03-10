80bcec4f43118a5cd6fd9319e71d7678ae14f496
Lijuan Wang <2738042w@student.gla.ac.uk>
2023-03-10 20:11:05 +0000

    update

--- takeaway_django_project/settings.py
+++ takeaway_django_project/settings.py
@@ -35,7 +35,8 @@ SECRET_KEY = "django-insecure-dagq)y(qx-vlc)*wn2+ub43=!(umvf%rrg-flx)w$l_uo&sene
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lijuanseeworld.pythonanywhere.com']
+ALLOWED_HOSTS = ['lijuanseeworld.pythonanywhere.com',
+                 '127.0.0.1']
 
 
 # Application definition
