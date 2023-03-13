354a9747b1ebd9c5dfbcf943d11fd74a4f942346
ChiyueCheng <123404318+ChiyueCheng@users.noreply.github.com>
2023-03-13 19:32:03 +0000

    Update urls.py

--- takeaway/urls.py
+++ takeaway/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("account/<username>/account.html", views.AccountView.as_view(), name="account"),
     path("takeaway/order_history.html", views.user_order_history, name="order_history"),
-    
+    path("takeaway/product/", views.product, name="product"),
 ]
