0a2687775a663c96de7e4a7bc6a5c7ef2ebb1907
Reeya2000 <127694981+Reeya2000@users.noreply.github.com>
2023-03-15 05:29:11 +0530

    Update urls.py

--- takeaway/urls.py
+++ takeaway/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path("addCart/", views.AddCartView.as_view(), name="addCart"),
     path("takeaway/order_history.html", views.user_order_history, name="order_history"),
     path("product/", views.product, name="product"),
+    path("cart/", views.user_cart, name="cart"),
+    path("checkout/", views.user_checkout, name="checkout"),
 ]
