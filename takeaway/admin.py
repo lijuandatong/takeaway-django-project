145664e6b60472a1617bfd07dd9fc6cff07c2f44
Lijuan Wang <2738042w@student.gla.ac.uk>
2023-03-16 19:46:32 +0000

    register model

--- takeaway/admin.py
+++ takeaway/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 
-from takeaway.models import UserProfile, Food, Wallet, Order, Comment, Cart, CartDetail
+from takeaway.models import UserProfile, Food, Wallet, Order, Comment, Cart, CartDetail, OrderDetail
 
 # Register your models here.
 
 admin.site.register(UserProfile)
 admin.site.register(Food)
 admin.site.register(Wallet)
-# admin.site.register(Order)
+admin.site.register(Order)
+admin.site.register(OrderDetail)
 admin.site.register(Comment)
 admin.site.register(Cart)
 admin.site.register(CartDetail)
