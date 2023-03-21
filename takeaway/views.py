b6bfeaa8572e9e4c6add7ef3d79a372b960dea9a
Reeya2000 <127694981+Reeya2000@users.noreply.github.com>
2023-03-22 00:26:57 +0530

    Update views.py

--- takeaway/views.py
+++ takeaway/views.py
@@ -295,3 +295,60 @@ class CartView(View):
                         }
         return render(request, 'takeaway/checkout.html', context_dict)
 
+
+@login_required
+def add_to_cart(request, slug):
+    item = get_object_or_404(Food, slug=slug)
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item,
+        user=request.user,
+        ordered=False
+    )
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+            messages.info(request, "Item qty was updated.")
+            return redirect('takeaway/cart.html')
+        else:
+            order.items.add(order_item)
+            messages.info(request, "Item was added to your cart.")
+            return redirect('takeaway/cart.html')
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(
+            user=request.user, ordered_date=ordered_date)
+        order.items.add(order_item)
+        messages.info(request, "Item was added to your cart.")
+    return redirect('takeaway/cart.html')
+
+
+@login_required
+def remove_from_cart(request, slug):
+    item = get_object_or_404(Food, slug=slug)
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            order.items.remove(order_item)
+            messages.info(request, "Item was removed from your cart.")
+            return redirect('takeaway/cart.html')
+        else:
+            # add a message saying the user dosent have an order
+            messages.info(request, "Item was not in your cart.")
+            return redirect('takeaway/cart.html', slug=slug)
+    else:
+        # add a message saying the user dosent have an order
+        messages.info(request, "u don't have an active order.")
+        return redirect('takeaway/index.html', slug=slug)
+    return redirect('takeaway/index.html', slug=slug)
