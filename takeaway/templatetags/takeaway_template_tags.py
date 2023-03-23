9485c496ac5c375ee2bb7fa2076956b3e3e324f0
Lijuan Wang <2738042w@student.gla.ac.uk>
2023-03-23 00:58:43 +0000

    fix header cart display

---
+++ takeaway/templatetags/takeaway_template_tags.py
@@ -0,0 +1,30 @@
+from django import template
+from django.contrib.auth.models import User
+
+from takeaway.models import Cart, CartDetail
+
+register = template.Library()
+
+
+@register.inclusion_tag('takeaway/header.html')
+def getCartCount(user=None):
+    cart_count = 0
+
+    if user is not None:
+        print("name不为空")
+
+        try:
+            cart = Cart.objects.get(user=user)
+        except Cart.DoesNotExist:
+            print("购物车发生异常")
+            cart_count = 0
+
+        cart_detail_list = CartDetail.objects.filter(cart=cart)
+        for cart_detail in cart_detail_list:
+            cart_count += cart_detail.count
+
+        print("购物车数量：" + str(cart_count))
+
+    return {'cart_count': cart_count,
+            'user': user}
+
